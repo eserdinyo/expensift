8be8412f57214d21692feab2c5080453d6483cb1
nadal <muhammeteser29@gmail.com>
2019-05-05 16:12:06 +0300

    added route and navlink

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Link, NavLink } from "react-router-dom";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
@@ -8,31 +8,39 @@ const ExpenseDashboardPage = () => (
   <div>This is from my dashboard component</div>
 );
 
-const AddExpensePage = () => (
+const AddExpensePage = () => <div>This is from add expense component</div>;
+
+const EditPage = () => <div>Edit Page</div>;
+
+const HelpPage = () => <div>Help page</div>;
+
+const ErrorPage = () => (
   <div>
-    This is from add expense component
+    404- <Link to="/">Go home</Link>
   </div>
 );
 
-const EditPage = () => (
-  <div>
-    Edit Page
-  </div>
-);
-
-const HelpPage = () => (
-  <div>
-    Help page
-  </div>
+const Header = () => (
+  <header>
+    <h1>Expensify</h1>
+    <NavLink to='/' activeClassName="is-active" exact={true}>Dashboard</NavLink>
+    <NavLink to='/create' activeClassName="is-active" >Create</NavLink>
+    <NavLink to='/edit' activeClassName="is-active" >Edit</NavLink>
+    <NavLink to='/help' activeClassName="is-active" >Help</NavLink>
+  </header>
 );
 
 const routes = (
   <BrowserRouter>
     <div>
-      <Route path="/" component={ExpenseDashboardPage} exact={true} />
-      <Route path="/create" component={AddExpensePage}  />
-      <Route path='/edit' component={EditPage} />
-      <Route path='/help' component={HelpPage} />
+      <Header />
+      <Switch>
+        <Route path="/" component={ExpenseDashboardPage} exact={true} />
+        <Route path="/create" component={AddExpensePage} />
+        <Route path="/edit" component={EditPage} />
+        <Route path="/help" component={HelpPage} />
+        <Route path="" component={ErrorPage} />
+      </Switch>
     </div>
   </BrowserRouter>
 );
